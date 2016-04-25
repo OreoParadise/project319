a2293019b7ce268882a085461f6b28d415367afe
Hassaluk-Jaja <casnovyai@hotmail.co.th>
2016-04-25 13:09:32 +0700

    correct something

--- src/view/Receipt.java
+++ src/view/Receipt.java
@@ -56,7 +56,7 @@ public class Receipt extends JFrame{
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        checkBillButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Oriopun Jaja\\Desktop\\OreoParadise\\project319\\src\\view\\checkBill.png")); // NOI18N
+        checkBillButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/checkBill.png"))); // NOI18N
         getContentPane().add(checkBillButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 570, 130, 40));
 
         totalPriceLabel.setFont(new java.awt.Font("Times New Roman", 3, 20)); // NOI18N
@@ -84,10 +84,10 @@ public class Receipt extends JFrame{
         jLabel2.setText("Receipt");
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 240, -1, -1));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\Oriopun Jaja\\Desktop\\OreoParadise\\project319\\src\\view\\receipt.png")); // NOI18N
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/receipt.png"))); // NOI18N
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 190, -1, -1));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Oriopun Jaja\\Desktop\\OreoParadise\\project319\\src\\view\\BG.png")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/BG.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 920, -1));
 
         pack();
