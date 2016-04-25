f03c32db9131101fdcfcc495d207ebb284341803
juiz007 <james_june@windowslive.com>
2016-04-25 19:25:41 +0700

    create some methods for dealing with adding mouse event in BillTable

--- src/view/BillTable.java
+++ src/view/BillTable.java
@@ -5,6 +5,7 @@
  */
 package view;
 
+import java.awt.event.MouseAdapter;
 import javax.swing.JLabel;
 import javax.swing.JToggleButton;
 import javax.swing.JButton;
@@ -47,9 +48,9 @@ public class BillTable extends JFrame {
         public void mouseClicked(java.awt.event.MouseEvent evt) {
             Table3MouseClicked(evt);
         }
-    });
-    */
-
+    });*/
+    
+    
     Table2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/Check.png"))); // NOI18N
     /*
     Table2.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -258,19 +259,94 @@ public class BillTable extends JFrame {
     pack();
     
     }
+    //Method for clicked event in table toggle buttons
+    public void setClickedTable1(MouseAdapter m){
+        Table1.addMouseListener(m);
+    }
+    public void setClickedTable2(MouseAdapter m){
+        Table2.addMouseListener(m);
+    }
+    public void setClickedTable3(MouseAdapter m){
+        Table3.addMouseListener(m);
+    }
+    public void setClickedTable4(MouseAdapter m){
+        Table4.addMouseListener(m);
+    }
+    public void setClickedTable5(MouseAdapter m){
+        Table5.addMouseListener(m);
+    }
+    public void setClickedTable6(MouseAdapter m){
+        Table6.addMouseListener(m);
+    }    
+    public void setClickedTable7(MouseAdapter m){
+        Table7.addMouseListener(m);
+    }    
+    public void setClickedTable8(MouseAdapter m){
+        Table8.addMouseListener(m);
+    }    
+    public void setClickedTable9(MouseAdapter m){
+        Table9.addMouseListener(m);
+    }    
+    public void setClickedTable10(MouseAdapter m){
+        Table10.addMouseListener(m);
+    }    
+    public void setClickedTable11(MouseAdapter m){
+        Table11.addMouseListener(m);
+    }    
+    public void setClickedTable12(MouseAdapter m){
+        Table12.addMouseListener(m);
+    }
+    
+    //methods for getting table toggle buttons for using in controller
+    public JToggleButton getTable1(){
+        return Table1;
+    }    
+    public JToggleButton getTable2(){
+        return Table2;
+    }    
+    public JToggleButton getTable3(){
+        return Table3;
+    }    
+    public JToggleButton getTable4(){
+        return Table4;
+    }    
+    public JToggleButton getTable5(){
+        return Table5;
+    }    
+    public JToggleButton getTable6(){
+        return Table6;
+    }    
+    public JToggleButton getTable7(){
+        return Table7;
+    }    
+    public JToggleButton getTable8(){
+        return Table8;
+    }    
+    public JToggleButton getTable9(){
+        return Table9;
+    }    
+    public JToggleButton getTable10(){
+        return Table10;
+    }    
+    public JToggleButton getTable11(){
+        return Table11;
+    }    
+    public JToggleButton getTable12(){
+        return Table12;
+    }
     
     // Variables declaration - do not modify                     
-    private javax.swing.JToggleButton Table1;
-    private javax.swing.JToggleButton Table10;
-    private javax.swing.JToggleButton Table11;
-    private javax.swing.JToggleButton Table12;
-    private javax.swing.JToggleButton Table2;
-    private javax.swing.JToggleButton Table3;
-    private javax.swing.JToggleButton Table4;
-    private javax.swing.JToggleButton Table5;
-    private javax.swing.JToggleButton Table6;
-    private javax.swing.JToggleButton Table7;
-    private javax.swing.JToggleButton Table8;
-    private javax.swing.JToggleButton Table9;
-    private javax.swing.JLabel jLabel2;
+    private JToggleButton Table1;
+    private JToggleButton Table10;
+    private JToggleButton Table11;
+    private JToggleButton Table12;
+    private JToggleButton Table2;
+    private JToggleButton Table3;
+    private JToggleButton Table4;
+    private JToggleButton Table5;
+    private JToggleButton Table6;
+    private JToggleButton Table7;
+    private JToggleButton Table8;
+    private JToggleButton Table9;
+    private JLabel jLabel2;
 }
